1788e2ee7183a00c15003201c0729e57a3975619
HeiligerBlindDarm <65182598+HeiligerBlindDarm@users.noreply.github.com>
2020-05-31 22:48:20 +0200

    Update PlayerMovement.cs

--- ProjectDynamind/Assets/Scripts/PlayerMovement.cs
+++ ProjectDynamind/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
@@ -9,22 +10,42 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeigth = 3f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
-    }
+    Vector3 velocity;
+    bool isGrounded;
+
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f * Time.deltaTime;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward;
+        Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeigth * -2f + gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);        
 
     }
 }
