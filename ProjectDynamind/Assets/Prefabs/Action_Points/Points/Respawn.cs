24857fbe4f74f4b8a51f70edeee1564a8d6d3316
Ramog <ramon.fischer1@gmx.de>
2020-06-03 20:23:51 +0200

    Reimplemented Action Points

---
+++ ProjectDynamind/Assets/Prefabs/Action_Points/Points/Respawn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public string PlayerName = "Player";
+    public bool isDead = false, isResetting = false;
+    GameObject player;
+
+    //TODO: See if somehow I can use the data-type Check_Point (would give a reference to the current check Point too)
+    public Vector3 currentCheckpointPosition;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find the player Object 
+       // player = GameObject.Find(PlayerName);
+        player = GameObject.FindWithTag("Player");
+
+        //set the current Checkpoint position to the position of the SpawnPoint
+        currentCheckpointPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: See if I can use Triggers or events
+        if (isResetting)
+        {
+            //set players position and the checkpoints position to the spawnpoints position
+            player.transform.position = currentCheckpointPosition = gameObject.transform.position;
+            isResetting = false;
+        }
+        else if (isDead)
+        {
+            //set the players position the last checkpoints position
+            player.transform.position = currentCheckpointPosition;
+            isDead = false;
+        }
+    }
+}
---
+++ ProjectDynamind/Assets/Prefabs/Action_Points/Render_Boxes.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// Only Runs when Unity is in Editor Mode
+[ExecuteInEditMode]
+public class Render_Boxes : MonoBehaviour
+{
+   
+    GameObject[] gameObjects;
+    public Color SpawnColor, CheckpointColor, EndpointColor, StandardColor, FrameColor;
+
+
+
+    //Draws A cube arround every Triggerzone
+    void OnDrawGizmos()
+    { 
+
+        Transform[] Transforms = GetComponentsInChildren<Transform>();
+
+        foreach (Transform child in Transforms)
+        {
+            if (child != null)
+            {
+
+                switch (child.name)
+                {
+                    case string a when a.Contains("Spawn_Point"):
+                        Gizmos.color = SpawnColor;
+                        break;
+                    case string a when a.Contains("Check_Point"):
+                        Gizmos.color = CheckpointColor;
+                        break;
+                    case string a when a.Contains("End_Point"):
+                        Gizmos.color = EndpointColor;
+                        break;
+                    default:
+                        Gizmos.color = StandardColor;
+                        break;
+                }
+
+                //states before its drawn what dimensions the cube has
+                Gizmos.matrix = child.localToWorldMatrix;
+
+                //Draws the cube that is specified in Gizmos.matrix
+                Gizmos.DrawCube(Vector3.zero, Vector3.one);
+                Gizmos.color = FrameColor;
+                Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+
+            }
+        }
+    
+    }
+}
