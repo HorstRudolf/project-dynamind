9dd538b1d76072302fda7c22cde83d8c61b5b60c
Ramog <ramon.fischer1@gmx.de>
2020-06-06 19:02:38 +0200

    Added Deathzones, checkpoints, spawnpoints, finishpoints and zonetriggers

---
+++ ProjectDynamind/Assets/Prefabs/Action_Points/Points/End_Point.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(BoxCollider))]
+public class End_Point : MonoBehaviour
+{
+    private BoxCollider boxCollider;
+    public string nextScene;
+    
+    public string trigger_Message;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set BoxCollider to is Triggerd so Objects don't Collide
+        boxCollider = GetComponent<BoxCollider>();
+        boxCollider.isTrigger = true;
+
+        //if the scene is empty throw an except because then nothing can be loaded
+        if (nextScene == "")
+            throw new NullReferenceException($"You need to set a Scene in {gameObject.name}. Scene was null");
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            string adress = "";
+            Transform currentObject = gameObject.transform;
+
+            //Print out the complete adress of Objects (parents of child)
+            while (currentObject != null) 
+            {
+                adress = $"{currentObject.name}/{adress}";
+                currentObject = currentObject.transform.parent;
+            } 
+
+            Debug.Log($"Sent by \"{adress}\": load:{nextScene}");
+
+
+            //Load the scene thats name is the String in nextScene 
+            //Important!!! has to be added under File->Build Settings...->Scenes in Build
+            SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ ProjectDynamind/Assets/Prefabs/Action_Points/Render_Boxes.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// Only Runs when Unity is in Editor Mode
+[ExecuteInEditMode]
+public class Render_Boxes : MonoBehaviour
+{
+    public Color SpawnColor, CheckpointColor, EndpointColor, StandardColor, DeathColor, DestructionColor, FrameColor;
+
+
+
+    //Draws A cube around every trigger-zone
+    void OnDrawGizmos()
+    { 
+
+        Transform[] Transforms = GetComponentsInChildren<Transform>();
+
+        foreach (Transform child in Transforms)
+        {
+            if (child != null)
+            {
+                switch (child.name)
+                {
+                    case string a when a.Contains("Spawn_Point"):
+                        Gizmos.color = SpawnColor;
+                        break;
+                    case string a when a.Contains("Check_Point"):
+                        Gizmos.color = CheckpointColor;
+                        break;
+                    case string a when a.Contains("End_Point"):
+                        Gizmos.color = EndpointColor;
+                        break;
+                    case string a when a.Contains("Death_Zone"):
+                        Gizmos.color = DeathColor;
+                        break;
+                    case string a when a.Contains("Destruction_Zone"):
+                        Gizmos.color = DestructionColor;
+                        break;
+                    default:
+                        Gizmos.color = StandardColor;
+                        break;
+                }
+
+                //states before its drawn what dimensions the cube has
+                Gizmos.matrix = child.localToWorldMatrix;
+
+                //Draws the cube that is specified in Gizmos.matrix
+                Gizmos.DrawCube(Vector3.zero, Vector3.one);
+                Gizmos.color = FrameColor;
+                Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+            }
+        }
+    
+    }
+}
