3a9e84a26843eb99311307295fb1ddda6aedd8ad
Ramog <ramon.fischer1@gmx.de>
2020-06-08 22:19:40 +0200

    Added Door and PressurePlate

---
+++ ProjectDynamind/Assets/Prefabs/Door/Door.cs
@@ -0,0 +1,50 @@
+﻿using Boo.Lang.Environments;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    float journeyLenght, startTime;
+    public float speed = 1;
+    public Vector3 endPos, startPos;
+    public AnimationCurve speeds;
+
+    public float distanceCovered;
+
+    bool open = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        endPos = transform.position + new Vector3(0, - transform.lossyScale.y, 0);
+        journeyLenght = Vector3.Distance(transform.position, endPos);
+        
+    }
+
+    public void Open()
+    {
+        open = true;
+    }
+
+    public void Close()
+    {
+        open = false;
+    }
+        
+        
+    // Update is called once per frame
+    void Update()
+    {
+        distanceCovered = Vector3.Distance(transform.position, endPos) / journeyLenght ;
+
+        if (open && transform.position.y > endPos.y)
+            transform.position += new Vector3(0, -speeds.Evaluate(distanceCovered), 0) * Time.deltaTime; 
+        else if (!open && transform.position.y < startPos.y)
+            transform.position += new Vector3(0, speeds.Evaluate(distanceCovered), 0) * Time.deltaTime;
+    }
+}
---
+++ ProjectDynamind/Assets/Prefabs/PressurePlate/PressurePlate.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressurePlate : MonoBehaviour
+{
+    public float PlateSpeed = 1.3f;
+    public UnityEvent onUnpress, onPress;
+
+    bool pressed = false;
+    int colCount = 0;
+    Vector3 oldPos;
+
+    void Start()
+    {
+        oldPos = gameObject.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+            colCount++;        
+        else if (col.CompareTag("Heavy"))
+            colCount++;
+     
+        if (colCount > 0 && !pressed)
+        {
+            onPress.Invoke();
+            pressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.CompareTag("Player"))
+            colCount--;
+
+        else if (col.CompareTag("Heavy"))
+            colCount--;
+
+        if (colCount <= 0 && pressed)
+        {
+            onUnpress.Invoke();
+            pressed = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pressed)
+        {
+            if (gameObject.transform.position.y > ( oldPos.y -gameObject.transform.lossyScale.y/2))
+                transform.position += new Vector3(0, -PlateSpeed, 0) * gameObject.transform.lossyScale.y * Time.deltaTime;
+        }
+        else
+        {
+            if(gameObject.transform.position.y < oldPos.y)
+                transform.position += new Vector3(0, PlateSpeed, 0) * gameObject.transform.lossyScale.y * Time.deltaTime;
+        }
+    }
+}
