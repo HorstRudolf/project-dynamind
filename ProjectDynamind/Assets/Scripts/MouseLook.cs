50035e0addf5f523f8c629fa65f8e0249ed5cb1e
HeiligerBlindDarm <65182598+HeiligerBlindDarm@users.noreply.github.com>
2020-06-14 16:30:42 +0200

    MouseScript kommentiert

--- ProjectDynamind/Assets/Scripts/MouseLook.cs
+++ ProjectDynamind/Assets/Scripts/MouseLook.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-
+    // Controll speed of mouse
     public float mouseSensitivity = 100f;
 
     public Transform playerBody;
@@ -15,16 +15,20 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // That you stay on screen with the mouse
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Process user input
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
+
+        // That the view cannot be rotated 360 degrees over the Y-axis
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
