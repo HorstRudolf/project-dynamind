4f4f9d3a7e3ee89dbeea7ea2212ba26e9a297b1b
HeiligerBlindDarm <65182598+HeiligerBlindDarm@users.noreply.github.com>
2020-06-25 22:07:36 +0200

    LadderPrefab und Script

--- ProjectDynamind/Assets/Scripts/PlayerMovement.cs
+++ ProjectDynamind/Assets/Scripts/PlayerMovement.cs
@@ -11,11 +11,10 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController controller;
-    [SerializeField]
-    private LayerMask ladderLayer;
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeigth = 3f;
+
 
     public static Vector3 velocity;
     bool isGrounded;
@@ -25,7 +24,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     public Status currentStatus = Status.Walking;
 
+    Transform playerTransform;
 
+    Vector3 position;
 
 
 
@@ -33,9 +34,29 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         CheckStatus();
-
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ladder")
+        {
+            //Collider col = other;
+            //Debug.Log("x: " + col.gameObject.transform.rotation.x +" y: " + col.gameObject.transform.rotation.y + " x: " + col.gameObject.transform.rotation.z);
+            //playerTransform.rotation = Quaternion.Euler(col.gameObject.transform.rotation.x, col.gameObject.transform.rotation.y, col.gameObject.transform.rotation.z);
+            currentStatus = Status.LadderClimbing;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Ladder")
+        {
+            //Collider col = other;
+            //Debug.Log("x: " + col.gameObject.transform.rotation.x + " y: " + col.gameObject.transform.rotation.y + " x: " + col.gameObject.transform.rotation.z);
+            //playerTransform.rotation = Quaternion.Euler(0, col.gameObject.transform.rotation.y, 0);
+            currentStatus = Status.Walking;
+        }
+    }
 
 
     void CheckStatus()
@@ -50,63 +71,47 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void CheckForLadder()
-    {
-        Collider[] hitColliders = Physics.OverlapBox(controller.gameObject.transform.position, transform.localScale / 2, Quaternion.identity, ladderLayer);
-
-        if (hitColliders.Length > 0)
-            currentStatus = Status.LadderClimbing;
-
-    }
-
     public void LadderMovement()
     {
+        playerTransform = controller.gameObject.transform;
 
-        // Problem so lange man bei Leiter ist, gibt es Kollisionen...
-        Collider[] hitColliders = Physics.OverlapBox(controller.gameObject.transform.position, transform.localScale / 2, Quaternion.identity, ladderLayer);
-
-        if (hitColliders.Length > 0)
+        if (Input.GetKey("w"))
         {
-            GameObject Ladder = hitColliders[0].gameObject;
-            GameObject PlayerObject = controller.gameObject;
-
-
-            if (Ladder.transform.localScale.y + Ladder.transform.position.y >= PlayerObject.transform.position.y + PlayerObject.transform.localScale.y)
-            {
-                PlayerObject.transform.rotation = Ladder.transform.rotation;
-                Debug.Log("Leiter Höhe + Position: " + (Ladder.transform.localScale.y + Ladder.transform.position.y) + " > SpielerPosition: " + (PlayerObject.transform.position.y + PlayerObject.transform.localScale.y));
-                // Process user input
-                float z = Input.GetAxis("Vertical");
-
-                Vector3 move1 = new Vector3(0, z);
-
-                // Move player
-                controller.Move(move1 * speed * Time.deltaTime);
-                return;
-            }
-
-            Debug.Log("geht rein");
-            float width = Ladder.transform.localScale.z;
-            //float height = Ladder.transform.localScale.y;
-            PlayerObject.transform.rotation = Quaternion.Euler(0, PlayerObject.transform.rotation.y, 0);
-            PlayerObject.transform.position = new Vector3(PlayerObject.transform.position.x, PlayerObject.transform.position.y + 0.1f, PlayerObject.transform.position.z + width + 0.10f);
-           // Vector3 move = new Vector3(0, 0, width + 0.10f);
-            
+            float z = Input.GetAxis("Vertical");
+            Vector3 move = transform.up * z;
 
             // Move player
-           // controller.Move(move * speed * Time.deltaTime);
-            currentStatus = Status.Walking;
-            hitColliders = null;
-
+            controller.Move(move * speed * Time.deltaTime);
         }
+        else if (Input.GetKey("s"))
+        {
+            float z = Input.GetAxis("Vertical");
+            Vector3 move = transform.up * z;
 
+            // Move player
+            controller.Move(move * speed * Time.deltaTime);
+
+            if (playerTransform.gameObject.transform.position.y == position.y)
+            {
+                currentStatus = Status.Walking;
+            }
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+
+            velocity.y = Mathf.Sqrt(jumpHeigth * -2f * gravity);
+            velocity.z = -3;
+            velocity.x = -3;
+            // fall speed
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+        position = new Vector3(0, playerTransform.gameObject.transform.position.y, 0);
     }
 
 
     public void PlayerMove()
     {
-
-
         isGrounded = controller.isGrounded;
 
         if (isGrounded && velocity.y < 0)
@@ -133,8 +138,6 @@ public class PlayerMovement : MonoBehaviour
         // fall speed
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-
-        CheckForLadder();
     }
 
 
