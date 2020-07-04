1176462f1f51aacf69a8577f2d1576cbb4a2c17d
HeiligerBlindDarm <65182598+HeiligerBlindDarm@users.noreply.github.com>
2020-07-04 15:31:17 +0200

    Kameraverhalten bei LadderMovement angepasst

--- ProjectDynamind/Assets/Scripts/MouseLook.cs
+++ ProjectDynamind/Assets/Scripts/MouseLook.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net.Sockets;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MouseLook : MonoBehaviour
 {
@@ -11,27 +12,70 @@ public class MouseLook : MonoBehaviour
     public Transform playerBody;
 
     float xRotation = 0f;
+    float yRotation = 0f;
+
+    PlayerMovement.Status currentStatus = PlayerMovement.currentStatus;
 
     // Start is called before the first frame update
     void Start()
     {
         // That you stay on screen with the mouse
         Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+
+        CheckStatus();
+
+    }
+
+    void LadderMouseLook()
     {
         // Process user input
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
+        yRotation += mouseX;
+
+        // That the view cannot be rotated 360 degrees over the Y-axis
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        yRotation = Mathf.Clamp(yRotation, -60f, 60f);
+
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+
+    void NormalMouseLook()
+    {
+
+        // Process user input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        yRotation += mouseX;
 
         // That the view cannot be rotated 360 degrees over the Y-axis
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    void CheckStatus()
+    {
+        if (currentStatus == PlayerMovement.Status.Walking)
+        {
+            NormalMouseLook();
+        }
+        else if (currentStatus == PlayerMovement.Status.LadderClimbing)
+        {
+            LadderMouseLook();
+        }
     }
 }
