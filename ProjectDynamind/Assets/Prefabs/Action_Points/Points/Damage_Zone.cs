a13e9d4d6bd949446e2bf7f02e92107fa48c8431
Ramog <ramon.fischer1@gmx.de>
2020-07-12 00:20:13 +0200

    Added damage zone. Added posibility to trigger more than just one animation with a lever.

---
+++ ProjectDynamind/Assets/Prefabs/Action_Points/Points/Damage_Zone.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Damage_Zone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    BoxCollider BoxCollider;
+    public bool triggered = false;
+    float lastDamage;
+    PlayerMovement playerMovement;
+
+    public int damageAmount = 1;
+    public float pause = 1;
+
+
+    void Start()
+    {
+        BoxCollider = GetComponent<BoxCollider>();
+        BoxCollider.isTrigger = true;
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+
+
+        lastDamage = Time.time;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player") && Time.time - lastDamage >= pause)
+        {
+            string adress = "";
+
+            Transform currentObject = gameObject.transform;
+
+            while (currentObject != null)
+            {
+                adress = $"{currentObject.name}/{adress}";
+                currentObject = currentObject.transform.parent;
+            }
+
+            Debug.Log($"Sent by \"{adress}\": Damage Zone Entered: {gameObject.name}");
+        }
+    }
+
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.CompareTag("Player") && Time.time - lastDamage >= pause)
+        {
+            lastDamage = Time.time;
+            playerMovement.TakeDamage(damageAmount);
+        }
+    }
+}
--- ProjectDynamind/Assets/Prefabs/bridge/LeverInteraction.cs
+++ ProjectDynamind/Assets/Prefabs/bridge/LeverInteraction.cs
@@ -9,16 +9,22 @@ public class LeverInteraction : MonoBehaviour
 {
     public float reachForAction = 4.0f;
     public string triggerName;
-    public GameObject bridge, lever;
+    public GameObject lever;
+    public GameObject[] bridge;
     GameObject player;
-    Animator animator;
+    Animator[] animator;
     public string tagOfObject = "Player";
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag(tagOfObject);
-        animator = bridge.GetComponent<Animator>();
+        animator = new Animator[bridge.Length];
+
+        for (int n = 0; n < bridge.Length; n++)
+        {
+            animator[n] = bridge[n].GetComponent<Animator>();
+        }        
     }   
 
     private void OnMouseOver()
@@ -27,7 +33,10 @@ public class LeverInteraction : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && Vector3.Distance(player.transform.position, gameObject.transform.position) < reachForAction)
         {
             lever.transform.rotation = Quaternion.Euler(lever.transform.rotation.eulerAngles.x, lever.transform.rotation.eulerAngles.y + 180, lever.transform.rotation.eulerAngles.z);
-            animator.SetTrigger(triggerName);
+
+
+            foreach (Animator ani in animator)
+                ani.SetTrigger(triggerName);
         }
     }
 }
